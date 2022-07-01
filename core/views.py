747df5c9a72f34f0c47f1be80af628bf63e18916
Nico FP <ni.friz@duocuc.cl>
2022-07-01 19:07:13 -0400

    Reparación de registro

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
+from django.contrib import messages
 from django.forms import PasswordInput
 from django.shortcuts import redirect, render
 from .forms import ProductoForm, CustomUserForm
 from .models import Producto
 from django.contrib.auth.decorators import login_required, permission_required
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import authenticate, login
 from .serializers import ProductoSerializer
 
 # Rest_framework
@@ -21,26 +22,33 @@ def flores(request):
     return render(request,'core/flores.html')
 def jardineria(request):
     return render(request,'core/jardineria.html')
-def login(request):
+def login_user(request):
     return render(request,'core/templates/registration/login.html')
 def macetero(request):
     return render(request,'core/macetero.html')
 def productos(request):
     return render(request,'core/productos.html')
 def registro(request):
-    data = {
-        'form' :CustomUserForm()
-    }
-    if request.method == 'POST':
+    data = {'form' :CustomUserForm()}
+    if request.method == "POST":
         formulario = CustomUserForm(request.POST)
         if formulario.is_valid():
-            formulario.save();
+            formulario.save()
             username = formulario.cleaned_data['username']
             password = formulario.cleaned_data['password1']
             user = authenticate(username=username, password=password)
             login(request, user)
-        else : return redirect(to='home')  
-    return render(request, 'registration/registro.html', data)
+            messages.success(request, 'Registro Exitoso')
+            return redirect('home')  
+            # data['form'] = formulario
+    else:         
+        formulario = CustomUserForm()
+    return render(request, 'registration/registro.html', {
+        'form':formulario,
+    })
+
+
+
 
 
 def tierrahoja(request):
